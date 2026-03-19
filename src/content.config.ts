588f084171c561de317215ca569e87c094af8892
Alex <github@dobrovansky.com>
2026-03-19 13:15:09 +1100

    actually installed a dev enviroment to test things before commiting this time...

--- src/content.config.ts
+++ src/content.config.ts
@@ -10,8 +10,7 @@ const blog = defineCollection({
 		title: z.string(),
 		description: z.string(),
 		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
+		
 		heroImage: z.string().optional(),
 	}),
 });
